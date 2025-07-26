35f867c9ee4ead442d1261aa15957eefd4996955
manoranjan <monoranjan.design@gmail.com>
2025-07-26 23:39:33 +0530

    added 3 static pages

--- resources/views/pages/frontend/static/terms-of-application.blade.php
+++ resources/views/pages/frontend/static/terms-of-application.blade.php
@@ -38,8 +38,121 @@
     <!-- ================ Header Section ================ -->
     @include('includes.frontend.header')
 
-    <section>
-        {{-- Contents --}}
+     <section class="innerConBodyOuter staticPages">
+        <div class="container">
+            <h2>Terms of Application</h2>
+        <dt>Last updated: July 16, 2025</dt>        
+        
+        <p>By agreeing to these Terms of Application (such as by clicking an acceptance
+button), you, acting individually and on behalf of the business entity listed in the
+application (together, the “Applicant”), confirm and agree to the following
+conditions.</p> 
+       
+     <h3>PLEASE NOTE THIS AGREEMENT INCLUDES AN ARBITRATION CLAUSE, A
+CLASS ACTION WAIVER, AND A WAIVER OF JURY TRIAL RIGHTS. YOU MAY
+OPT OUT AS DESCRIBED BELOW.</h3>
+        <ol>
+            <li>You represent that you have the authority to submit this application and
+any related documents on behalf of yourself and the Applicant, and to
+provide the consents and authorizations described herein.</li>
+            <li>This application is submitted to Verifen Funding LLC (“Verifen Funding”)
+and its third-party lenders, finance companies, and other entities
+involved in providing commercial financing or purchasing receivables
+(collectively, the “Recipients”).</li>
+            <li>Upon approval of this application, you are authorized to execute financing
+agreements on behalf of the Applicant. Note that certain financing
+products may require a personal guaranty.</li>
+            <li>You confirm all information submitted to Verifen Funding and/or
+Recipients is truthful, complete, and accurate, and you will promptly
+notify Verifen Funding of any changes to such information.</li>
+            <li>Verifen Funding and Recipients may rely on the information provided for
+any decisions or actions related to this application.</li>
+            <li>Except for consumer credit reports, Verifen Funding may share your
+application information and documents with Recipients to facilitate your
+financing request.</li>
+            <li>Verifen Funding and Recipients may exchange information about you and
+the Applicant for purposes related to administration, marketing, and
+servicing, as permitted by law. Your personal information may also be
+shared with the Applicant during underwriting.</li>
+            <li>You and the Applicant release and waive any claims against any party
+providing information about you or the Applicant.</li>
+            <li>You agree to the terms of the Verifen Funding Credit Gathering
+Authorization [link here: www.verifen.com/credit-gatheringauthorization],
+which authorizes Verifen Funding and Recipients to obtain
+credit reports and other relevant information regarding the Applicant.</li>
+            <li>Dispute Resolution and Arbitration Agreement:
+To resolve any disputes efficiently and economically, you and Verifen
+Funding agree that any dispute may be submitted to binding arbitration
+rather than court litigation (“Agreement to Arbitrate”). Arbitration is a less
+formal process than a lawsuit, involves a neutral arbitrator instead of a
+judge or jury, and limits court review. The arbitrator may award the same
+remedies a court could. Arbitration will be conducted on an individual
+basis only; class or representative claims are prohibited. The arbitrator’s
+authority to grant relief is limited to the individual claimant’s needs.
+    <ul>
+        <li>You waive any right to participate in a class action or representative
+proceeding against Verifen Funding.</li>
+        <li>This arbitration agreement covers all disputes between you and
+Verifen Funding, including those based on contract, tort, statute,
+fraud, or any other legal theory, regardless of when such claims
+arose.</li>
+        <li>Exceptions: Nothing in this Agreement waives your or Verifen
+Funding’s right to pursue enforcement through governmental
+agencies, seek injunctive relief in court, or bring intellectual property
+infringement claims in court.</li>
+        <li>Arbitration will be administered by the American Arbitration
+Association (“AAA”) under its Commercial Dispute Resolution
+Procedures (“AAA Rules”), as modified herein. The AAA Rules and
+forms are available at <a href="www.adr.org" target="_blank">www.adr.org</a> or by calling 1-800-778-7879.</li>
+        <li>To initiate arbitration, you must send a written Notice of Dispute by
+certified mail to:
+Verifen Funding LLC, Legal Department, 3044 Coney Island Avenue,
+Mailbox #7, Brooklyn, NY 11235.
+The Notice must include a description of the dispute and the relief
+requested. If not resolved within 30 days, arbitration may proceed.</li>
+        <li>For claims up to $10,000, Verifen Funding will reimburse your
+arbitration filing fees upon request unless the claim is deemed
+frivolous, in which case you must reimburse fees paid by Verifen
+Funding.</li>
+        <li>You may choose whether arbitration is conducted based on
+documents only, by phone, or in person. In-person hearings will be
+held in Brooklyn, NY.</li>
+        <li>The arbitrator will provide a written decision with findings and
+conclusions.</li>
+        <li>Class or representative arbitrations are prohibited unless both parties
+agree otherwise.</li>
+        <li><strong>Opt-Out:</strong> New users may reject this arbitration agreement by mailing
+a signed written Opt-Out Notice postmarked within 30 days of first
+accepting these Terms to:
+Verifen Funding LLC, Legal Department, 3044 Coney Island Avenue,
+Mailbox #7, Brooklyn, NY 11235.
+The notice must include your name, address, username/email
+associated with your account, and signature.</li>
+        <li>If you opt out, the arbitration clause will not apply to you, but all other
+Terms will remain effective.</li>
+        <li>Any future changes to this arbitration agreement may be rejected by
+written notice within 30 days, which will result in immediate
+termination of your account.</li>
+        <li>If any part of this arbitration provision is found invalid or
+unenforceable, the remaining provisions will remain in effect. If the
+entire provision is unenforceable, disputes will be resolved in court as
+outlined in Section 15 of these Terms.</li>        
+    </ul>
+        </li>            
+        </ol>
+
+        <p><strong>IMPORTANT:</strong> This application is for commercial financing or purchase of future
+receivables only. Funds may not be used for personal, family, or household
+purposes.</p>
+        
+        <p><strong>Federal Law Notice:</strong></p>        
+        <p>Financial institutions are required to verify the identity of individuals and entities
+opening accounts or establishing relationships. This includes collecting name,
+address, date of birth, Social Security or tax identification numbers, and other
+information to confirm your identity. You may be asked to provide supporting
+documents.</p>
+       
+    </div>
     </section>
 
     <!-- ================ Footer Section ================ -->
--- resources/views/pages/frontend/static/terms-of-use.blade.php
+++ resources/views/pages/frontend/static/terms-of-use.blade.php
@@ -38,8 +38,224 @@
     <!-- ================ Header Section ================ -->
     @include('includes.frontend.header')
 
-    <section>
-        {{-- Contents --}}
+     <section class="innerConBodyOuter staticPages">
+        <div class="container">
+            <h2>Verifen Funding Terms of Use</h2>
+        <dt>Effective Date: July 16, 2025</dt>        
+        <h3>Welcome and Agreement Overview</h3>
+        <p>This document sets forth the agreement between you and Verifen Funding LLC
+(“Verifen Funding,” “we,” or “us”) regarding your use of our website
+<a href="www.verifen.com">www.verifen.com</a> (the “Site”) and any related services (“Services”). By accessing
+or using our Site or Services, you agree to comply with these Terms and our
+Privacy Policy. If you do not agree, please discontinue use immediately.</p>      
+    <h3>1. Our Service Offering and Scope</h3>
+     <p>Verifen Funding provides business-focused services designed to connect small
+business owners with financial institutions. We may update, change, or
+discontinue services at any time.</p>   
+         <ul>
+            <li><strong>Funding Match Platform:</strong>We facilitate connections between eligible
+businesses and lenders, credit unions, and other funding sources.</li>
+            <li><strong>Expanding Services:</strong>Additional offerings may be introduced, which will
+be subject to these Terms.</li>            
+       </ul>
+     <h3>2. User Responsibilities and Site Usage</h3>
+        <p>By registering or using Verifen Funding’s Site and Services, you agree to:</p>
+        <ul>
+            <li>Use your account exclusively for your own business needs.</li>
+            <li>Maintain accurate and truthful information.</li>
+            <li>Assume responsibility for interactions with third parties.</li>
+            <li>Recognize that matches or funding are not guaranteed.</li>
+            <li>Comply with all applicable laws and regulations.</li>
+            <li>Use Services solely for legitimate business purposes.</li>
+       </ul>
+        <h3>3. Intellectual Property and Content Rights</h3>        
+       <ul>
+            <li>Verifen Funding owns all intellectual property related to the Site and
+Services.</li>
+            <li>Users may not copy, reproduce, or create derivative works from any
+proprietary material.</li>
+            <li>By submitting content, you grant us a worldwide, royalty-free license to
+use and distribute your submissions for service purposes.</li>            
+       </ul>
+        <h3>4. External Links and Third-Party Relationships</h3>
+        <p>Our Site may include links to third-party websites or advertisers. These are
+provided for convenience and do not constitute endorsements. We are not
+responsible for third-party content or transactions.</p>       
+        <h3>5. Service Warranty and Availability Disclaimer</h3>
+        <p>The Site and Services are provided “as is” without warranties. We do not
+guarantee uninterrupted or error-free service and disclaim liability for third-party
+content.</p>   
+
+        <h3>6. Limiting Our Liability</h3>       
+        <p>To the fullest extent permitted by law, Verifen Funding disclaims liability for
+damages related to your use of the Services, with our maximum financial
+responsibility capped at $25 or the fees you have paid.</p>
+        <h3>7. Your Agreement to Protect Us</h3>
+        <p>You agree to indemnify and defend Verifen Funding and its affiliates from any
+claims arising out of your use of our Site or Services, or your violation of these
+Terms or applicable laws.</p>
+        <h3>8. How We Handle Your Information</h3>
+        <ul>
+            <li>Your use of our Site and Services is subject to our Privacy Policy.</li>
+            <li>We may disclose information to comply with legal obligations or to
+protect rights.</li>
+            <li>We employ security measures to protect your data.</li>
+            <li>We may analyze anonymized data to improve our Services.</li>
+            <li>By submitting content, you grant us a license to use it as needed.</li>            
+        </ul>
+        <h3>9. Prohibited Activities and Usage Restrictions</h3>
+        <p>You may not:</p>
+        <ul>
+            <li>Use the Services as a money transmitter or for unauthorized resale.</li>
+            <li>Act on behalf of third parties.</li>
+            <li>Misuse or abuse the Services.</li>
+            <li>Violate agreements with payment providers linked to the Services.</li>
+            <li>Attempt to replicate or derive new services from ours.</li> 
+            <li>Use the Services for personal bank accounts or illegal purposes.</li>           
+        </ul>
+        <h3>10. Resolving Disputes Through Arbitration</h3>
+        <p>We agree to resolve disputes via binding arbitration, foregoing class actions and
+jury trials. Arbitration will follow AAA rules, with certain exceptions.</p>
+     
+    <p>New users may opt out of arbitration by submitting a written notice within
+        30 days to:</br>
+        Verifen Funding LLC</br>
+        3044 Coney Island Avenue, Mailbox #7</br>
+        Brooklyn, NY 11235
+        </p>
+        <h3>11. Telephone Communications and Consent to Contact</h3>
+    <ul>
+        <li><strong>Call Monitoring and Recording.</strong> You acknowledge and agree that
+telephone calls made to or from Verifen Funding LLC, including its agents
+and affiliates, may be recorded or monitored for quality assurance and
+training purposes.</li>
+        <li><strong>Accuracy of Contact Details.</strong> You affirm that any contact information
+you provide to Verifen Funding, including your name, mailing address,
+email address, and telephone numbers (business, residential, or mobile),
+is accurate and belongs to you. You confirm you are the rightful
+subscriber or owner of all phone numbers submitted. Providing a phone
+number that is not yours is strictly prohibited. Should any of your contact
+details change, including ownership of any telephone numbers, you agree
+to notify us promptly before the change occurs by either replying STOP
+to any text message you receive from us, calling (888) 788-6160, or
+sending written notice via email to privacy@verifen.com.</li>
+        <li><strong>Consent to Automated Communications.</strong> By voluntarily providing your
+phone number(s), you expressly consent to receive autodialed,
+prerecorded, or artificial voice calls and text messages (including SMS,
+MMS, or future messaging technologies) from Verifen Funding and its
+authorized representatives concerning promotions, your account,
+transactions, or your relationship with us. This consent applies even if
+your number is listed on any state or federal Do Not Call registry. You also
+consent to be contacted via any email, mailing address, or phone number
+you or your representative provide, or that we obtain through lawful
+means such as caller ID or skip tracing. This consent continues even if
+your account is closed or relationship with us ends, unless you opt out as
+described below. Consent to receive automated promotional
+communications is not required to purchase products or services.</li>
+        <li><strong>Opt-Out Procedures.</strong> You may opt out of receiving automated text
+messages at any time by replying STOP to any text message received
+from us. Reply HELP for assistance. Please note that messaging may
+come from multiple shortcodes; replying STOP to one shortcode does not
+unsubscribe you from all. To opt out of automated voice calls, you must
+provide written notice to privacy@verifen.com including your full name,
+mailing address, account number, and the phone number(s) for which
+you revoke consent. It may take up to 30 days to process your opt-out
+request. Non-automated calls may still be made to you. Please be aware
+that third parties who have your contact details may still contact you
+despite your opt-out request; we are not responsible for those
+communications.</li>
+        <li><strong>Charges for Messages and Calls.</strong> Receiving automated calls or texts
+from Verifen Funding does not incur a fee from us. However, standard
+message and data rates may apply per your phone carrier’s plan, which
+you are responsible for. Please check your wireless plan for details.</li>
+        <li><strong>Unauthorized Phone Use.</strong> You agree to promptly notify Verifen Funding
+if your telephone device is lost, stolen, or if you suspect unauthorized
+use. Although we are not liable for losses from unauthorized use, you may
+be responsible for any resulting damages.</li>
+        <li><strong>Your Indemnification.</strong> You agree to indemnify and hold Verifen Funding
+harmless from any claims, liabilities, or expenses (including reasonable
+attorney fees) arising from your providing inaccurate or unauthorized
+telephone information or failure to update contact details, including
+claims under the Telephone Consumer Protection Act or equivalent laws.</li>
+        <li><strong>Release of Claims.</strong> You release Verifen Funding from all claims,
+damages, or liabilities related to telephone calls or text messages you
+receive from us, including those alleging violations of federal or state
+telemarketing, privacy, or consumer protection laws.</li>
+        <li><strong>General Responsibilities.</strong> You are responsible for obtaining and
+maintaining all equipment, devices, and services necessary to receive
+calls and messages. Availability of text messaging may vary by carrier
+and device compatibility. Your obligations under this section survive
+termination of these Terms.</li>
+        <li><strong>Text Message Service Terms.</strong> By opting in to Verifen Funding’s text
+message service (“SMS Service”), you accept these terms, including
+binding arbitration and waiver of class actions as outlined in Section 10
+(“Dispute Resolution by Binding Arbitration”). You authorize Verifen
+Funding to send marketing and service messages to the phone number
+you provide, using autodialing or manual methods. Opting in is not a
+condition of purchase or service.</li>
+        <li>You affirm you are authorized to use the phone number provided. After
+opting in, you may receive welcome or administrative messages,
+including requests to confirm your opt-in. You may cancel the service
+anytime by texting STOP, after which you will receive a confirmation
+message and no further texts will be sent unless you re-subscribe.</li>
+        <li>For help, text HELP or contact support@verifen.com or call (888)
+788-6160. Carrier delays or failures in message delivery are not our
+responsibility.</li>
+        <li>Message and data rates may apply. You may receive up to 15 messages
+per month. Contact your wireless provider for details.</li>
+        <li>For privacy questions, see: https://www.verifen.com/privacy-policy/</li>        
+    </ul>
+   
+        <h3>12. Governing Law and Legal Jurisdiction</h3>
+        <p>These Terms and your use of Verifen Funding’s Site and Services are governed by
+the laws of the State of New York, without regard to conflict of law principles. You
+agree that any legal disputes will be resolved exclusively in the federal or state
+courts located in Kings County, New York.</p>
+    <p>You acknowledge that breaches of these Terms may cause irreparable harm,
+entitling Verifen Funding to seek injunctive relief or other equitable remedies
+without posting bond.</p>
+    <p>These Terms, the Privacy Policy, and any applicable payment or service terms
+constitute the entire agreement between you and Verifen Funding, superseding
+any prior agreements.</p>
+    <p>FAQs are for informational purposes and are not part of this agreement.
+Provisions of these Terms will survive termination. Failure to enforce any provision
+does not waive rights.</p>
+    <p>If any provision is invalid, the rest remain effective.</p>
+    <p>All claims or actions related to these Terms or use of the Site must be filed within
+one year of the event or be barred.</p>
+    <p>Section titles are for reference only and have no legal effect.</p>
+
+    <h3>13. Ownership and Protection of Content</h3>
+    <p>All visual and textual elements on <a href="www.verifen.com">www.verifen.com</a>, including graphics, logos,
+trademarks, sound, and code (“Content”), are owned by Verifen Funding and
+protected by intellectual property laws. Unauthorized use of such Content may
+result in legal action.</p>
+    <h3>14. Confidentiality and Security</h3>
+    <p>We take reasonable measures to protect your account information but cannot
+guarantee absolute security, especially if you share credentials with others. You
+acknowledge your responsibility for safeguarding your information.
+Use of our Services is subject to our Privacy Policy.</p>
+        <h3>15. Updates to Terms of Use</h3>
+        <p>Verifen Funding may update these Terms to reflect changes in our operations,
+policies, or legal requirements.</br>
+Material changes will be communicated via email to your registered address or
+through notices on the Site.</br>
+Please review these Terms periodically. The effective date at the top reflects the
+latest revision.</p>
+        <h3>16. Termination and Account Suspension</h3>
+        <p>This agreement becomes effective once you use the Site or Services and remains
+in effect until terminated.</br>
+Verifen Funding may suspend or terminate your account at any time, with or
+without cause, and with or without notice.</br>
+We reserve the right to retain your account information to comply with legal or
+regulatory obligations.</br>
+If suspended for violation of these Terms, fees paid are non-refundable and any
+pending payments become due immediately.</br>
+You may terminate your account through the procedures described in the
+“Cancellations” section.</br>
+Sections 10 (Dispute Resolution) and 11 (Telephone Communications) will survive
+termination.</p>
+    </div>
     </section>
 
     <!-- ================ Footer Section ================ -->
