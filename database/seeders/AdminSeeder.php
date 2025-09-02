980c2c2243284b0901c24f52589770f8d7ff39d5
Surajit Sil <surajit@gmail.com>
2025-09-03 00:20:47 +0530

    fix

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
